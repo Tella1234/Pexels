3bb402f1ca30d399057aedeba865d62d4fa07ad9
Tella Boluwatife <boluwatifetella@gmail.com>
2021-12-15 06:00:32 -0800

    Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,33 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '320px',
+        'md': '425px',
+        'lg': '768px',
+        'xl': '1024px',
+        '2xl': '1440px',
+      },
       fontFamily: {
         nunito: "'Nunito Sans', sans-serif",
       },
       spacing: {
         '97': '28rem',
         '100': '40rem',
+        '101': '9px',
+        '102': '6px',
+        '103': '8px',
       },
       colors: {
         jgreen: {
           light: '#044a2f',
           DEFAULT: '#05a081',
           dark: '#0ac77e'
+        },
+        navb: {
+          light: '#232a34',
+          DEFAULT: '#232a34',
+          dark: '#232a34',
         }
       }
     },
