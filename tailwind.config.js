c963302881478d895e8e47c196734452c917af86
Tella Boluwatife <boluwatifetella@gmail.com>
2021-12-17 03:14:38 -0800

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'md': '425px',
         'lg': '768px',
         'xl': '1024px',
-        '2xl': '1440px',
+        '2xl': '1600px',
       },
       fontFamily: {
         nunito: "'Nunito Sans', sans-serif",
